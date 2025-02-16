7dde29e6e42b90728abc88ff643226dd004efe5f
lwauke <lwauke@hotmail.com>
2025-02-16 20:04:15 -0300

    feat: add on change listener on Form Group

--- src/components/FormGroup/FormGroup.tsx
+++ src/components/FormGroup/FormGroup.tsx
@@ -1,17 +1,18 @@
-import { HTMLInputTypeAttribute } from "react";
+import { HTMLInputTypeAttribute, ChangeEvent } from "react";
 import { Input, Label } from "./styles";
 
 interface FormGroupProps {
   label: string;
   type: HTMLInputTypeAttribute;
   className?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function FormGroup({ label, type, className }: FormGroupProps) {
+function FormGroup({ label, type, className, onChange }: FormGroupProps) {
   return (
     <div className={className}>
       <Label>{label}</Label>
-      <Input type={type} />
+      <Input type={type} onChange={onChange}/>
     </div>
   )
 }
