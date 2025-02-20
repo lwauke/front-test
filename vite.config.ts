81b17ce459d686871d47e48fcbaa50a7cdc67c8f
lwauke <lwauke@hotmail.com>
2025-02-20 20:41:48 -0300

    ci: improve build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react(), tsConfigPaths(), ViteImageOptimizer()],
 });
